ebf4ade4677a6639818dbe671b60562897948f82
Ignacio Picun <picunignacio@gmail.com>
2017-01-24 14:34:25 -0300

    Fixed some minimal styling issues in resume

--- pages/resume.js
+++ pages/resume.js
@@ -32,13 +32,13 @@ const resume = css({
     display: 'flex',
     flexDirection: 'row',
     fontFamily: '"Roboto Mono", monospaced',
-    marginBottom: '30px',
+    marginBottom: '60px',
     '@media(max-width: 767px)': {
       flexDirection: 'column'
     },
     '& .section-title': {
+      marginTop: 0,
       '@media(min-width: 768px)': {
-        borderRight: '1px solid rgba(0, 0, 0,.3)',
         paddingRight: '30px',
         minWidth: '220px',
         textAlign: 'right'
@@ -50,15 +50,18 @@ const resume = css({
     '& .section-content': {
       flexGrow: 1,
       '@media(min-width: 768px)': {
+        borderLeft: '1px solid rgba(0, 0, 0,.3)',
         paddingLeft: '30px'
       },
       '& ul': {
         paddingLeft: '18px',
+        margin: 0,
         '& li:not(:last-of-type)': {
           marginBottom: '45px'
         }
       },
       '& h2': {
+        marginTop: 0,
         fontSize: '19px',
         '& small': {
           fontSize: '12px',
