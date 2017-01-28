91db9dd77cd59e4b84f57ad8df9f3f43c56f1ef8
Ignacio Picun <picunignacio@gmail.com>
2017-01-28 00:35:20 -0300

    Fixed links target in projects and lab

--- components/resume-lab.js
+++ components/resume-lab.js
@@ -9,7 +9,7 @@ export default () =>
     <div className='section-content'>
       <ul>
         <li>
-          <h2><a href='https://github.com/ignaciopicun/vuepacker'>Vuepacker</a></h2>
+          <h2><a href='https://github.com/ignaciopicun/vuepacker' target='_blank'>Vuepacker</a></h2>
           <p>Vuepacker is a Full-featured Vue.js (Runtime only) setup with Wepack 2 including hot reloading, linting (standard), unit testing (jsdom, karma, mocha, chai) and css extraction.</p>
         </li>
       </ul>
--- components/resume-projects.js
+++ components/resume-projects.js
@@ -9,20 +9,20 @@ export default () =>
     <div className='section-content'>
       <ul>
         <li>
-          <h2><a href='http://www.smartcare.com/'>SmartCare</a></h2>
+          <h2><a href='http://www.smartcare.com/' target='_blank'>SmartCare</a></h2>
           <p>SmartCare is a child care management system.</p>
         </li>
         <li>
-          <h2><a href='https://www.mopals.com/'>Mopals</a></h2>
+          <h2><a href='https://www.mopals.com/' target='_blank'>Mopals</a></h2>
           <p>Mopals is a loyalty rewards program.</p>
           <p>It allows users to earn rewards for a number of activities including purchases, sharing, checking in, reviewing etc.</p>
         </li>
         <li>
-          <h2><a href='http://www.itagtip.com/'>iTagTip</a></h2>
+          <h2><a href='http://www.itagtip.com/' target='_blank'>iTagTip</a></h2>
           <p>Using iTagTip you can pick things you care about from a store. And it will deliver relevant tips when you are nearby those things.</p>
         </li>
         <li>
-          <h2><a href='http://www.curogenslearning.com/'>CuroGens Learning</a></h2>
+          <h2><a href='http://www.curogenslearning.com/' target='_blank'>CuroGens Learning</a></h2>
           <p>CuroGens Learning is an e-Learning platform that offers individualized learning paths for their users.</p>
         </li>
       </ul>
